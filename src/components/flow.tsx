b492a90abe54ce18f3ee6b497a1686c67af2f847
Rishabh Singh <ris8645208@gmail.com>
2024-10-02 20:12:58 +0530

    fix: add basic flow with react flow

--- src/components/flow.tsx
+++ src/components/flow.tsx
@@ -1,11 +1,71 @@
-import { ReactFlow, Controls, Background } from "@xyflow/react";
+import {
+  ReactFlow,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+  Edge,
+  Node,
+  Connection,
+  addEdge,
+} from "@xyflow/react";
+import { useCallback } from "react";
+
+const initialNodes: Array<Node> = [
+  {
+    id: "1",
+    position: { x: 0, y: 0 },
+    data: { label: "Input" },
+    type: "input",
+    draggable: true,
+  },
+];
+
+const initialEdges: Array<Edge> = [];
 
 function Flow() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const addNode = () => {
+    const lastNode = nodes[nodes.length - 1];
+    const newNode: Node = {
+      id: String(nodes.length + 1),
+      position: { x: lastNode.position.x + 200, y: lastNode.position.y },
+      data: { label: "New Node" },
+      draggable: true,
+    };
+    setNodes([...nodes, newNode]);
+  };
+
+  const onConnect = useCallback(
+    (params: Connection) => setEdges((els) => addEdge(params, els)),
+    [setEdges]
+  );
+
   return (
-    <div style={{ height: "100%" }}>
-      <p>Flow component</p>
-      <div style={{ height: "80vh", position: "relative" }}>
-        <ReactFlow>
+    <div className="h-full">
+      <div className="">
+        <h1 className="text-3xl font-bold text-center">Flow</h1>
+        <div className="flex justify-center">
+          <button
+            onClick={addNode}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Add Node
+          </button>
+        </div>
+      </div>
+      <div className="h-[80vh]">
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          nodesConnectable={true}
+          nodesDraggable={true}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+        >
           <Background />
           <Controls />
         </ReactFlow>
